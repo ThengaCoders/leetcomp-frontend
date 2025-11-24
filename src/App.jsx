7cc10eda672fb70c328a6acfc090bdbc8f061c59
jefin <techy.jefinjoji@gmail.com>
2025-11-24 22:25:17 +0530

    feat: add client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Search from './components/Search'
+import Create from './components/Create'
+import MyRooms from './components/MyRooms'
+import Room from './components/Room'
+import NotFound from './components/NotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="app-root">
+        <Header />
+
+        <main style={{flex: 1, padding: '1rem'}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms/search" element={<Search />} />
+            <Route path="/rooms/create" element={<Create />} />
+            <Route path="/rooms" element={<MyRooms />} />
+            <Route path="/rooms/:id" element={<Room />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
