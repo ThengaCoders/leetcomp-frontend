265bc954d1740ad446f300e71aba60e89b54cd70
devavinod <devavinod1947@gmail.com>
2025-11-26 21:18:53 +0530

    finieshed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 export default function Header() {
-  const activeStyle = { fontWeight: '700', textDecoration: 'underline' }
+ 
 
-  return (
-    <header style={{padding: '1rem', borderBottom: '1px solid #ddd', display:'flex', gap: '1rem', alignItems:'center'}}>
-      <h1 style={{margin:0}}>MyApp</h1>
-      <nav style={{marginLeft: '1rem'}}>
-        <NavLink to="/" style={({isActive}) => isActive ? activeStyle : undefined}>Home</NavLink>
-        {' | '}
-        <NavLink to="/rooms/search" style={({isActive}) => isActive ? activeStyle : undefined}>Search</NavLink>
-        {' | '}
-        <NavLink to="/rooms/create" style={({isActive}) => isActive ? activeStyle : undefined}>Create</NavLink>
-        {' | '}
-        <NavLink to="/rooms/" style={({isActive}) => isActive ? activeStyle : undefined}>MyRooms</NavLink>
-        {' | '}
-        <NavLink to="/rooms/1" style={({isActive}) => isActive ? activeStyle : undefined}>Room 1</NavLink>
-      </nav>
-    </header>
-  )
+  return (
+    <header className={styles.siteHeader}>
+      <div className={styles.logo}>
+      	<NavLink to="/" className={styles.logoLink}>
+      		<span className={styles.logoSymbol}>
+    <i className="fa-solid fa-code"></i>
+</span>
+      		<span className={styles.logoText}>LeeComp</span>
+      	</NavLink>
+      </div>
+      <nav className={styles.mainNav}>
+        
+        {/* Use className function to conditionally add the 'active' class */}
+        <NavLink 
+          to="/rooms/" 
+          className={({ isActive }) => 
+            isActive 
+              ? `${styles.navLink} ${styles.active}` 
+              : styles.navLink
+          }
+        >
+          MyRooms
+        </NavLink>
+        
+        <NavLink 
+          to="/signin" 
+          className={({ isActive }) => 
+            isActive 
+              ? `${styles.navLink} ${styles.active}`
+              : styles.navLink
+          }
+        >
+          Sign In
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
